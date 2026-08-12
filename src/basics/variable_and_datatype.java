8a6c42917f89d5ef6325170115f49b70d1c3de3c
Krish Maurya <yashmaurya370@gmail.com>
2026-08-12 22:10:13 +0530

    learned variables and data types in java

---
+++ src/basics/variable_and_datatype.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class variable_and_datatype {
+    static void main() {
+         // Integers --> byte , short , int , long
+        byte a=5;
+        short b=55;
+        int c= 567;
+        long d=98767556;
+
+        // Real numbers --> float , double
+
+        float x=5.4f;
+        double y=6.98;
+
+        char r='a';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(r);
+
+
+    }
+}
